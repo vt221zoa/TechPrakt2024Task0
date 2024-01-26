02636b827e1194952dd2edb847b096a519b37033
Oleksandr Zaloznyi <vt221_zoa@student.ztu.edu.ua>
2024-01-26 20:58:33 +0200

    Додано C# проект та обчислення суми, а також переконано що gitignore спрацював і не додані непотрібні файли.

---
+++ Project_sum/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введіть перше число:");
+if (double.TryParse(Console.ReadLine(), out double number1))
+{
+    Console.WriteLine("Введіть друге число:");
+    if (double.TryParse(Console.ReadLine(), out double number2))
+    {
+        double sum = number1 + number2;
+        Console.WriteLine($"Сума чисел {number1} та {number2} дорівнює: {sum}");
+    }
+    else
+    {
+        Console.WriteLine("Помилка при зчитуванні другого числа.");
+    }
+}
+else
+{
+    Console.WriteLine("Помилка при зчитуванні першого числа.");
+}
